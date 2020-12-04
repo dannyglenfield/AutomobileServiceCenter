8978386c32372972f32997ddf10dcc150cd0f1d0
Danny Glenfield <danny.glenfield@gmail.com>
2020-12-04 17:51:18 -0500

    Add _SecureLayout template and Dashboard stub page.

--- AutomobileServiceCenter.Web/wwwroot/js/init.js
+++ AutomobileServiceCenter.Web/wwwroot/js/init.js
@@ -1,8 +1,13 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+
+      $('.collapsible').collapsible();
+      $('.collapsible.expandable').collapsible({
+        accordion: false
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
